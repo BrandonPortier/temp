31a9e7140cd715fe0d3ece9f15013484958b1a34
DrBotic <ezralboerman@gmail.com>
2024-09-28 00:29:03 -0400

    backend structure

---
+++ Archive/Back-end/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.ledgerlegends.com',
+    'https://ledgerlengends.com',
+    'http://localhost:3500'
+]
+
+module.exports = allowedOrigins
---
+++ Archive/Back-end/routes/userRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require("../controllers/usersController");
+
+// Route to get all users and create new users
+
+
+// Route for getting a user by their username
+
+
+// Route for getting a user by their email
+
+
+// Route for username/password login check
+
+
+// Route to update user info
+
+
+// Route to update user password
+
+
+// Route to update user role
+
+
+// Route to update user active status
+
+
+// Route to update user suspension status
+
+
+module.exports = router;
